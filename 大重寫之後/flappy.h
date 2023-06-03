075702a5ba82e3135ef26d51e8c9b675ce847225
Lin_Xuanyou <134621816+TsukiSama9292@users.noreply.github.com>
2023-06-03 19:25:49 +0800

    Add files via upload

--- 大重寫之後/flappy.h
+++ 大重寫之後/flappy.h
@@ -1,12 +1,13 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <bits/stdc++.h>
-//#include "button.h"
+#include "set_rank.h"
 #define TEAMNAME "111_2 NO.5 TEAM - Flappy bird"
 #define SOUND_HIT "./AllSundries/sound/hit.wav"
 #define SOUND_ADD "./AllSundries/sound/add.wav"
 #define SOUND_BGM "./AllSundries/sound/bgm.wav"
 #define SOUND_WING "./AllSundries/sound/wing.wav"
+#define SOUND_NO1 "./AllSundries/sound/no1.wav"
 #define BACKGROUND "./AllSundries/image/background.png"
 #define BIRD "./AllSundries/image/bird.png"
 #define PIPE "./AllSundries/image/pipe.png"
@@ -21,6 +22,7 @@ using namespace std;
 using namespace sf;
 enum Status{_homepage, _inputname, _game, _gameover, _showrank, _introduce};
 class FlappyBird : protected setrank{
+	private:
 	Event event;
 	Texture btn_start,btn_enter,btn_restart,btn_homepage,btn_showrank,btn_introduce;
 	vector<Sprite> vsbtn;
@@ -29,8 +31,8 @@ class FlappyBird : protected setrank{
 	RenderWindow *window;
 	float y,g,frame,interval;
 	int count, score , bgm_times;
-	SoundBuffer buffer_hit,buffer_wing,buffer_add,buffer_bgm;
-	Sound sound_hit,sound_wing,sound_add,sound_bgm;
+	SoundBuffer buffer_hit,buffer_wing,buffer_add,buffer_bgm,buffer_no1;
+	Sound sound_hit,sound_wing,sound_add,sound_bgm,sound_no1;
 	Font font;
 	Text text_score,text_rank;
 	Texture bg, bd, pipe,btn;
@@ -38,6 +40,7 @@ class FlappyBird : protected setrank{
 	vector<Sprite> pipes;
 	bool gameover, addscore;
 	Status gamestatus = _homepage;
+	string name;
 	/*protected:
 		void pipeMove();
 		void birdAnime();
@@ -52,13 +55,11 @@ class FlappyBird : protected setrank{
 		void run();
 		void asd();
 	*/
-public:
-	FlappyBird(){ //建構元(子)，設定初始值 
-	  setSFML(); //由於SFML套件的物件設定不可在建構子執行，所以另外呼叫函數 
-	}
 	void setSFML(){ //用於設定初始值
+		name="Steven";
 		//參數設定 
-		g =  frame = 0.f; //設定初始重量、幀(用於設定鳥的圖示與傾角) 
+		g = 2.0f;
+		frame = 0.f; //設定初始重量、幀(用於設定鳥的圖示與傾角) 
 		interval = 240.f; //設定管道區間 
 		count = 0; //常數-用於生成管道
 		bgm_times = 0; //設定背景音樂播放之正記數 
@@ -84,7 +85,7 @@ public:
 		if(!bd.loadFromFile(BIRD))//載入圖片
 			cout<<"Fail loading bird.png"<<endl;  
 		bird = new Sprite(); //建立精靈 
-		setPictureSize(bird,&bd,2.f,3,500,150);
+		setPictureSize(bird,&bd,2.5f,3,500,75);
 		
 		/*以下是管道的圖與精靈設定*/
 		if(!pipe.loadFromFile(PIPE))//載入圖片 
@@ -117,10 +118,13 @@ public:
 			cout<<"Fail loading add.wav"<<endl;	//載入失敗_add
 		if(!buffer_bgm.loadFromFile(SOUND_BGM)) //載入音效_bgm
 			cout<<"Fail loading bgm.wav"<<endl; //載入失敗_bgm
+		if(!buffer_no1.loadFromFile(SOUND_NO1)) //載入音效_no1
+			cout<<"Fail loading no1.wav"<<endl; //載入失敗_no1
 		sound_hit.setBuffer(buffer_hit); //音效播放器設定_hit 
 		sound_wing.setBuffer(buffer_wing); //音效播放器設定_wing
 		sound_add.setBuffer(buffer_add); //音效播放器設定_add
 		sound_bgm.setBuffer(buffer_bgm); //音效播放器設定_bgm 
+		sound_no1.setBuffer(buffer_no1); //音效播放器設定_no1
 		
 		sbtn_start=new Sprite();
 		sbtn_showrank=new Sprite();
@@ -149,7 +153,6 @@ public:
 		spt->setTextureRect(IntRect(0,0,txt->getSize().x/cut,txt->getSize().y));
 		spt->setPosition(px-spt->getGlobalBounds().width/2.f,py-spt->getGlobalBounds().height/2.f);
 	}
-	
 	void events(){ //用於關閉視窗，重新開始遊戲 
 		auto e = new Event(); //視窗事件物件設定 
 		while( window->pollEvent( *e ) ){ //事件發生時執行 
@@ -161,7 +164,6 @@ public:
 			
 		}
 	}
-	
 	void draw(){ //刷新畫面，渲染並顯示圖示 
 		window->clear(); //清除整個畫面，用於刷新畫面，達到動畫效果 
 		window->draw(*background); //待執行顯示背景 
@@ -172,16 +174,14 @@ public:
 		window->draw(text_score); //待執行顯示分數 
 		window->display(); //呼叫OpenGL渲染完成後調用(對當前幀的所有待顯示圖像顯示在畫面上) 
 	}
-	
 	void pipeMove(){ //用於移動水管，與鳥的跳躍 
-	
 		if(Keyboard::isKeyPressed(Keyboard::Space)&&bird->getPosition().y>25){ //高度25禁止往上
 			sound_wing.play(); //播放揮翅膀音效 
-			bird->setRotation(-frame - 10.f); //旋轉稍微向上看 
+			bird->setRotation(-frame*2 - 10.f); //旋轉稍微向上看 
 			g = -8.f; //設定重量向下 
 		}
 		else
-			bird->setRotation(frame - 10.f); //旋轉稍微向下看
+			bird->setRotation(frame*2 - 10.f); //旋轉稍微向下看
 		if( count % 150 == 0 ){ //每執行150次時 
 			int pos = rand() % 335 + 25; //設定隨機高度 
 			pipeTop->setPosition(1000, pos);  //上管道設定位置
@@ -212,17 +212,14 @@ public:
 				addscore = true; //設定已經加過分數了，避免上下管道都加分 
 			}else //另一個管道通過，將其設定還未加過分數，使下次能繼續加分 
 				addscore = false;
-		}
-	}
-	
+		}}
 	void birdAnime(){ //設定鳥的動畫 
 		frame += 0.15f; // 設定幀 
 		if( frame > 3 ){ // 當大於三
 			frame -= 3; // 設為-3，製造循環的感覺 
 		}	
 		bird->setTextureRect(IntRect( 34 * (int)frame, 0, 34, 24 )); //設定圖示取樣範圍 
-	}
-	
+	}	
 	void birdMove(){ //移動鳥 
 		bird->move(0, g); //向下墜落 
 		g += 0.5f; //逐漸增快 
@@ -231,41 +228,49 @@ public:
 			gameover = true; //遊戲結束
 			gamestatus = _gameover;
 		}
-	}
-	
+	}	
 	void gameAnime(){ //執行精靈移動與鳥的跳躍和動畫 
 		if(!gameover){ //還沒結束遊戲
 			birdAnime(); //呼叫鳥的動畫 
 			birdMove(); //呼叫鳥的位移 
 			pipeMove(); //呼叫管道的位移 
 		}
-	}
-	
-	void run(){ 
+	}	
+	void run(){ //執行遊戲主體之Function 
 		while(!gameover){ //當視窗還開著
 			events(); //呼叫函數events，用於關閉視窗，重新開始遊戲 
 			gameAnime(); //呼叫函數game，執行精靈移動與鳥的跳躍和動畫 
 			draw(); //呼叫函數draw，刷新畫面，渲染並顯示圖示
 			count++; //常數增加，用於管道生成
-			//bgmCircle();
+			bgmCircle();
 			if( count == 300 ){ //常數等於300 
 				count = 0; //常數重製
 				 //呼叫音樂重複播放 
 			}
 		}
 	}
-	void reset_Bird(){
+	void reset_Bird(){ //重新設定遊戲中鳥的精靈(圖像) 
 		bird->setTexture(bd);  //精靈圖案設為鳥 
 		bird->setPosition(500.f - bd.getSize().x / 2.f , 300.f-bd.getSize().y / 2.f); //設定鳥的初始位置 
 		bird->setScale(2.f, 2.f); //調整大小(比例)
 		bird->setTextureRect(IntRect(0, 0, 34, 24)); //取用圖片中，最左側的鳥 
 	}
+	void home_BirdAnime(){ //首頁中鳥的動畫 
+		frame += 0.15f; // 設定幀 
+		if( frame > 3 ) // 當大於三
+			frame -= 3; // 設為-3，製造循環的感覺 
+		bird->setTextureRect(IntRect( 34 * (int)frame, 0, 34, 24 )); //設定圖示取樣範圍
+		bird->move(0, g); //向下墜落 
+		if(bird->getPosition().y>90)
+			g-=0.05f;
+		else if(bird->getPosition().y<50)
+			g+=0.05f;
+	}
 	void bgmCircle(){ //音樂重複播放 
-		if(bgm_times>=1400){ //當count循環6次 重新播放bgm 
+		if(bgm_times>=1330){
 			bgm_times=0; //初始化bgm_times 
-			//sound_bgm.play(); //重新播放bgm 
+			sound_bgm.play(); //重新播放bgm 
 		}else{
-			cout<<bgm_times<<endl;
 			bgm_times++; //記數+1 
 		}
 	}
@@ -292,48 +297,23 @@ public:
 					while(window->pollEvent(event)){
 						if (event.type == sf::Event::Closed)
 			                window->close();
-						if (isMouseOver(sbtn_start)){
-							sbtn_start->setTextureRect(IntRect(btn_start.getSize().x/2.f,0,btn_start.getSize().x/2.f,btn_start.getSize().y)); //改成滑鼠在上圖片 
-							if (Mouse::isButtonPressed(Mouse::Left)){
-								gamestatus = _inputname;
-							}
-						}else{
-							sbtn_start->setTextureRect(IntRect(0,0,btn_start.getSize().x/2.f,btn_start.getSize().y));
-						}
-						if (isMouseOver(sbtn_showrank)){
-							sbtn_showrank->setTextureRect(IntRect(btn_showrank.getSize().x/2.f,0,btn_showrank.getSize().x/2.f,btn_showrank.getSize().y)); //改成滑鼠在上圖片 
-							if (Mouse::isButtonPressed(Mouse::Left)){
-								gamestatus = _showrank;
-							}
-						}else{
-							sbtn_showrank->setTextureRect(IntRect(0,0,btn_showrank.getSize().x/2.f,btn_showrank.getSize().y));
-						}
-						if (isMouseOver(sbtn_introduce)){
-							sbtn_introduce->setTextureRect(IntRect(btn_introduce.getSize().x/2.f,0,btn_introduce.getSize().x/2.f,btn_introduce.getSize().y)); //改成滑鼠在上圖片
-							if (Mouse::isButtonPressed(Mouse::Left)){
-								gamestatus = _introduce;
-							}
-						}else{
-							sbtn_introduce->setTextureRect(IntRect(0,0,btn_introduce.getSize().x/2.f,btn_introduce.getSize().y));
-						}
+						button_Start();
+						button_Rank();
+						button_Introduce();
 					}
+					home_BirdAnime();
 					window->draw(*sbtn_start);
 					window->draw(*sbtn_showrank);
 					window->draw(*sbtn_introduce);
+					window->draw(*bird);
 					window->display();
 					break;
 				case _inputname:
 					while(window->pollEvent(event)){
 						if(event.type == sf::Event::Closed)
 			                window->close();
-						if(isMouseOver(sbtn_enter)){
-							sbtn_enter->setTextureRect(IntRect(btn_enter.getSize().x/2.f,0,btn_enter.getSize().x/2.f,btn_enter.getSize().y)); //改成滑鼠在上圖片 
-							if (Mouse::isButtonPressed(Mouse::Left)){
-								gamestatus = _game;
-							}
-						}else{
-							sbtn_enter->setTextureRect(IntRect(0,0,btn_enter.getSize().x/2.f,btn_enter.getSize().y));
-						}
+			            //inputName();
+						button_Enter();
 					}
 					window->draw(*sbtn_enter);
 					window->display();
@@ -346,28 +326,15 @@ public:
 					pipes.clear(); //管道全部清除
 					bird->setPosition(500.f - bd.getSize().x / 2.f , 300.f-bd.getSize().y / 2.f); //初始化鳥的座標 
 					gameover = false; //設結束結束為否 
+					reset_Bird();
 					run();
 					break;
 				case _gameover:
 					while(window->pollEvent(event)){
 						if(event.type == sf::Event::Closed)
 			                window->close();
-						if(isMouseOver(sbtn_restart)){
-							sbtn_restart->setTextureRect(IntRect(btn_restart.getSize().x/2.f,0,btn_restart.getSize().x/2.f,btn_restart.getSize().y)); //改成滑鼠在上圖片
-							if (Mouse::isButtonPressed(Mouse::Left)){
-								gamestatus = _game;
-							}
-						}else{
-							sbtn_restart->setTextureRect(IntRect(0,0,btn_restart.getSize().x/2.f,btn_restart.getSize().y));
-						}
-						if(isMouseOver(sbtn_homepage)){
-							sbtn_homepage->setTextureRect(IntRect(btn_homepage.getSize().x/2.f,0,btn_homepage.getSize().x/2.f,btn_homepage.getSize().y)); //改成滑鼠在上圖片 
-							if (Mouse::isButtonPressed(Mouse::Left)){
-								gamestatus = _homepage;
-							}
-						}else{
-							sbtn_homepage->setTextureRect(IntRect(0,0,btn_homepage.getSize().x/2.f,btn_homepage.getSize().y));
-						}
+						button_Restart();
+						button_Homepage();
 					}	
 					for(auto &p:pipes){ //遍歷所有管道 
 						window->draw(p); //待執行顯示管道 
@@ -382,14 +349,7 @@ public:
 					while(window->pollEvent(event)){
 						if(event.type == sf::Event::Closed)
 			                window->close();
-						if(isMouseOver(sbtn_homepage)){
-							sbtn_homepage->setTextureRect(IntRect(btn_homepage.getSize().x/2.f,0,btn_homepage.getSize().x/2.f,btn_homepage.getSize().y)); //改成滑鼠在上圖片 
-							if (Mouse::isButtonPressed(Mouse::Left)){
-								gamestatus = _homepage;
-							}
-						}else{
-							sbtn_homepage->setTextureRect(IntRect(0,0,btn_homepage.getSize().x/2.f,btn_homepage.getSize().y));
-						}
+						button_Homepage();
 					}
 					window->draw(*sbtn_homepage);
 					window->display();
@@ -398,14 +358,7 @@ public:
 					while(window->pollEvent(event)){
 						if(event.type == sf::Event::Closed)
 			                window->close();
-						if(isMouseOver(sbtn_homepage)){
-							sbtn_homepage->setTextureRect(IntRect(btn_homepage.getSize().x/2.f,0,btn_homepage.getSize().x/2.f,btn_homepage.getSize().y)); //改成滑鼠在上圖片 
-							if (Mouse::isButtonPressed(Mouse::Left)){
-								gamestatus = _homepage;
-							}
-						}else{
-							sbtn_homepage->setTextureRect(IntRect(0,0,btn_homepage.getSize().x/2.f,btn_homepage.getSize().y));
-						}
+						button_Homepage();
 					}
 					window->draw(*sbtn_homepage);
 					window->display();
@@ -414,5 +367,80 @@ public:
 					break;
 			}
 		}
+	}
+	void button_Start(){
+		if (isMouseOver(sbtn_start)){
+			sbtn_start->setTextureRect(IntRect(btn_start.getSize().x/2.f,0,btn_start.getSize().x/2.f,btn_start.getSize().y)); //改成滑鼠在上圖片 
+			if (Mouse::isButtonPressed(Mouse::Left)){
+				gamestatus = _inputname;
+			}
+		}else{
+			sbtn_start->setTextureRect(IntRect(0,0,btn_start.getSize().x/2.f,btn_start.getSize().y));
+		}
+	}
+	void button_Rank(){
+		if (isMouseOver(sbtn_introduce)){
+			sbtn_introduce->setTextureRect(IntRect(btn_introduce.getSize().x/2.f,0,btn_introduce.getSize().x/2.f,btn_introduce.getSize().y)); //改成滑鼠在上圖片
+			if (Mouse::isButtonPressed(Mouse::Left)){
+				gamestatus = _introduce;
+			}
+		}else{
+			sbtn_introduce->setTextureRect(IntRect(0,0,btn_introduce.getSize().x/2.f,btn_introduce.getSize().y));
+		}
+	}
+	void button_Introduce(){
+		if (isMouseOver(sbtn_showrank)){
+			sbtn_showrank->setTextureRect(IntRect(btn_showrank.getSize().x/2.f,0,btn_showrank.getSize().x/2.f,btn_showrank.getSize().y)); //改成滑鼠在上圖片 
+			if (Mouse::isButtonPressed(Mouse::Left)){
+				gamestatus = _showrank;
+			}
+		}else{
+			sbtn_showrank->setTextureRect(IntRect(0,0,btn_showrank.getSize().x/2.f,btn_showrank.getSize().y));
+		}
+	}
+	void button_Enter(){
+		if(isMouseOver(sbtn_enter)){
+			sbtn_enter->setTextureRect(IntRect(btn_enter.getSize().x/2.f,0,btn_enter.getSize().x/2.f,btn_enter.getSize().y)); //改成滑鼠在上圖片 
+			if (Mouse::isButtonPressed(Mouse::Left)){
+				if( name[0] ==' '){
+					cout<<"name format error"<<endl;//show warning about name format error
+				}
+				else
+					gamestatus = _game;
+			}
+		}else{
+			sbtn_enter->setTextureRect(IntRect(0,0,btn_enter.getSize().x/2.f,btn_enter.getSize().y));
+		}
+	}
+	void button_Restart(){
+		if(isMouseOver(sbtn_restart)){
+			sbtn_restart->setTextureRect(IntRect(btn_restart.getSize().x/2.f,0,btn_restart.getSize().x/2.f,btn_restart.getSize().y)); //改成滑鼠在上圖片
+			if (Mouse::isButtonPressed(Mouse::Left)){
+				gamestatus = _game;
+			}
+		}else{
+			sbtn_restart->setTextureRect(IntRect(0,0,btn_restart.getSize().x/2.f,btn_restart.getSize().y));
+		}	
+	}
+	void button_Homepage(){
+		if(isMouseOver(sbtn_homepage)){
+			sbtn_homepage->setTextureRect(IntRect(btn_homepage.getSize().x/2.f,0,btn_homepage.getSize().x/2.f,btn_homepage.getSize().y)); //改成滑鼠在上圖片 
+			if (Mouse::isButtonPressed(Mouse::Left)){
+				gamestatus = _homepage;
+				g =  frame = 0.f;
+				setPictureSize(bird,&bd,2.5f,3,500,75);
+				bird->setRotation(0);
+			}
+		}else{
+			sbtn_homepage->setTextureRect(IntRect(0,0,btn_homepage.getSize().x/2.f,btn_homepage.getSize().y));
+		}
+	}
+	
+	public:
+	FlappyBird(){ //建構元(子)，設定初始值 
+	  setSFML(); //由於SFML套件的物件設定不可在建構子執行，所以另外呼叫函數 
+	}
+	void flappy_start(){
+		changeWindow();
 	}
 };
--- 大重寫之後/main.cpp
+++ 大重寫之後/main.cpp
@@ -1,12 +1,9 @@
-#include "set_rank.h"
-#include "set_rank.cpp"
 #include "flappy.h"
-
 using namespace std;
 int main(){
   srand(time(NULL));
   auto *flappybird = new FlappyBird();
-  flappybird->changeWindow();
+  flappybird->flappy_start();
   return 0;
 }
 
