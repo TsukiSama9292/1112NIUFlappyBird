1c3ef30adb1a7fcbc11a5698fbbf6033ddde399a
Lin_Xuanyou <134621816+TsukiSama9292@users.noreply.github.com>
2023-06-06 14:50:53 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "flappy.h"
 using namespace std;
 int main(){
-  srand(time(NULL));
+  srand(time(NULL)); //設定隨機變數種子 
   auto *flappybird = new FlappyBird(); //建立flappy物件，即執行遊戲 
   delete(flappybird);//刪除物件 
   return 0;
